25fa7b2eddf064809ea0988206af041e4a10897f
sun pochin <sunpochin@gmail.com>
2021-07-22 22:05:46 +0800

    adding product routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
-import products from './data/products.js';
 import connectDB from './config/db.js';
 // const express = require('express');
 // const dotenv = require('dotenv');
 // const products = require('./data/products');
+
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -17,14 +18,7 @@ app.get('/', (req, res) => {
 	res.send('api is running');
 });
 
-app.get('/api/products', (req, res) => {
-	res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
