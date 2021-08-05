0e3c2b8bdd4e32a8388b1b1296469fca0c4d545e
sun pochin <sunpochin@gmail.com>
2021-08-05 21:49:26 +0800

    adding loader and message

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 
-import {useDispatch, useSelector} from 'react-redux'
-import {listProducts} from '../actions/productActions'
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
 	// const [products, setProducts] = useState([]);
-	const dispatch = useDispatch()
+	const dispatch = useDispatch();
+	const productList = useSelector((state) => state.productList);
+	const { loading, error, products } = productList;
 
-	const products = []
-	 
 	useEffect(() => {
-		dispatch(listProducts() )
+		dispatch(listProducts());
 	}, [dispatch]);
 
 	return (
 		<div>
 			<h1>Latest Products</h1>
-			<Row>
-				{products.map((product) => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'> {error} </Message>
+			) : (
+				<Row>
+					{products.map((product) => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</div>
 	);
 };
