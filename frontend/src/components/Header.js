391febe46a1a35ee7cd7dc4824afe7ea64a0c02c
sun pochin <sunpochin@gmail.com>
2022-02-07 19:48:24 +0800

    updating brand

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,24 +8,22 @@ const Header = () => {
 			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
 				<Container>
 					<LinkContainer to='/'>
-						<Navbar.Brand>Pro shop</Navbar.Brand>
+						<Navbar.Brand>MERN Shop</Navbar.Brand>
 					</LinkContainer>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ms-auto'>
-						<LinkContainer to='/cart'>
-						<Nav.Link href='/cart'>
-								<i className='fas fa-shopping-cart'></i> Cart
-							</Nav.Link>
-							
-						</LinkContainer>
-						<LinkContainer to='/login'>
-						<Nav.Link href='/login'>
-								<i className='fas fa-user'></i>
-								Login
-							</Nav.Link>
-							
-						</LinkContainer>
+							<LinkContainer to='/cart'>
+								<Nav.Link href='/cart'>
+									<i className='fas fa-shopping-cart'></i> Cart
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<Nav.Link href='/login'>
+									<i className='fas fa-user'></i>
+									Login
+								</Nav.Link>
+							</LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
